47efa6664176b85ff72a668baf3b0605535e583d
Yochi <Yochi@gongyouqiangs-MacBook-Pro.local>
2018-08-24 15:00:11 +0800

    first commit

---
+++ Cocoa-KLine/Cocoa_KLine/Cocoa_KLine.h
@@ -0,0 +1,29 @@
+//
+//  Cocoa_KLine.h
+//  Cocoa-KLine
+//
+//  Created by Yochi on 2018/7/31.
+//  Copyright © 2018年 Yochi. All rights reserved.
+//
+
+#ifndef Cocoa_KLine_h
+#define Cocoa_KLine_h
+
+/** 数据模型 */
+#import "Cocoa_ChartModel.h"
+#import "Cocoa_ChartManager.h"
+
+/** 工具样式 */
+#import "Cocoa_ChartProtocol.h"
+#import "Cocoa_CalculateCoordinate.h"
+#import "Cocoa_ChartStylesheet.h"
+
+/** 指标 */
+#import "Cocoa_CandleLineView.h"
+#import "Cocoa_TecnnicalOptionsView.h"
+#import "Cocoa_TradingVolumeView.h"
+#import "Cocoa_MACDView.h"
+#import "Cocoa_OBVView.h"
+#import "Cocoa_KDJView.h"
+
+#endif /* Cocoa_KLine_h */
